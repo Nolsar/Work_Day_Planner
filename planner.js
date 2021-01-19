5bbab3c575297fa585212018c9c4eb742bbb813f
Sara Nolan <saraa.ann28@gmail.com>
2021-01-19 13:17:41 -0500

    get current hour

--- planner.js
+++ planner.js
@@ -1,17 +1,4 @@
-// variables
-var hour9 = $("#9");
-var hour10 = $("#10");
-var hour11 = $("#11");
-var hour12 = $("#12");
-var hour1 = $("#13");
-var hour2 = $("#14");
-var hour3 = $("#15");
-var hour4 = $("#16");
-var hour5 = $("#17");
-
-// WHEN I open the planner, the current day is displayed at the top of the calendar
-
-
+// WHEN I open the planner, the current day is displayed at the top of the calenda
 var monthNames = [
         "January", "February", "March",
         "April", "May", "June",
@@ -29,15 +16,31 @@ var days = [
 var date = days[(new Date()).getMonth()] + "," + ' ' + monthNames[(new Date()).getMonth()] + " " + (new Date()).getDate();
 $("#currentDay").text(date);
 
-// WHEN I scroll down
-// THEN I am presented with time blocks for standard business hours
-// WHEN I view the time blocks for that day
-// THEN each time block is color-coded to indicate whether it is in the past, present, or future
-        //function that changes color to red when it is in the past & blue current & green future
+// WHEN I scroll down, THEN I am presented with time blocks for standard business hours
+var currentHour = new Date();
+console.log(currentHour.getHours());
+
+// variables
+var hour9 = $("#9");
+var hour10 = $("#10");
+var hour11 = $("#11");
+var hour12 = $("#12");
+var hour1 = $("#13");
+var hour2 = $("#14");
+var hour3 = $("#15");
+var hour4 = $("#16");
+var hour5 = $("#17");
+
+// WHEN I view the time blocks for that day, THEN each time block is color-coded to indicate whether it is in the past, present, or future
+
+
+
+//function that changes color to red when it is in the past & blue current & green future;
+              
+
 
 // WHEN I click into a time block
 
-        //onclick event for time block
 
 // THEN I can enter an event
 
@@ -55,4 +58,3 @@ $("#currentDay").text(date);
 // THEN the saved events persist
 
         //cancel page refresh
-
